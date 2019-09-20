c6a38509f0ad77db9ee6fecbc6549c32c16e9a44
Zakir Gowani <zakirgowani@gmail.com>
2019-09-20 10:29:22 -0500

    -

--- src/components/GuppyWrapper/index.jsx
+++ src/components/GuppyWrapper/index.jsx
@@ -62,20 +62,24 @@ class GuppyWrapper extends React.Component {
   /**
    * This function takes two objects containing filters to be applied
    * and combines them into one filter object in the same format.
+   * Note: the admin filter takes precedence. Selected values in the user
+   * filter will be discarded if the key collides. This is to avoid
+   * the user undoing the admin filter. (Multiple user checkboxes increase the 
+   amount of data shown when combined, but an admin filter should always decrease
+   or keep constant the amount of dat shown when combined with a user filter).
   * */
-  mergeFilters(filterA, filterB) {
-    console.log('guppy mergeFilters. filterA: ', filterA);
-    console.log('guppy mergeFilters. filterB: ', filterB);
-    const filterAB = Object.assign({}, filterA);
-    for (const key in filterB) {
-      if (Object.prototype.hasOwnProperty.call(filterA, key)
-          && Object.prototype.hasOwnProperty.call(filterB, key)) {
+  mergeFilters(userFilter, adminAppliedPreFilter) {
+    console.log('guppy mergeFilters. userFilter: ', userFilter);
+    console.log('guppy mergeFilters. adminAppliedPreFilter: ', adminAppliedPreFilter);
+    const filterAB = Object.assign({}, userFilter);
+    for (const key in adminAppliedPreFilter) {
+      if (Object.prototype.hasOwnProperty.call(userFilter, key)
+          && Object.prototype.hasOwnProperty.call(adminAppliedPreFilter, key)) {
         console.log('mergeFilters 72: ', key);
-        filterAB[key].selectedValues = filterA[key].selectedValues.concat(
-          filterB[key].selectedValues,
-        );
-      } else if (Object.prototype.hasOwnProperty.call(filterB, key)) {
-        filterAB[key] = filterB[key];
+        // The admin filter overrides the user filter to maintain exclusivity.
+        filterAB[key].selectedValues = adminAppliedPreFilter[key].selectedValues;
+      } else if (Object.prototype.hasOwnProperty.call(adminAppliedPreFilter, key)) {
+        filterAB[key] = adminAppliedPreFilter[key];
       }
     }
     console.log('guppy mergeFilters. filterAB: ', filterAB);
