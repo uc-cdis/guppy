9b0f492cddb4709f82de9567101a8fa53b404f4d
Michael Ingram <mpingram@uchicago.edu>
2020-01-21 10:54:25 -0600

    Revert changes to middlewares/index.js

--- src/server/middlewares/index.js
+++ src/server/middlewares/index.js
@@ -1,20 +1,15 @@
 import authMiddleware from './authMiddleware';
 import tierAccessMiddleware from './tierAccessMiddleware';
 import config from '../config';
-import log from '../logger';
 
 const middlewares = [];
-log.info('[yeah] insidee middlewares index.js 6');
 switch (config.tierAccessLevel) {
   case 'libre':
-  log.info('[yeah] insidee middlewares index.js 9');
     break;
   case 'regular':
-    log.info('[yeah] insidee middlewares index.js 12');
     middlewares.push(tierAccessMiddleware);
     break;
   case 'private':
-    log.info('[yeah] insidee middlewares index.js 16');
     middlewares.push(authMiddleware);
     break;
   default:
