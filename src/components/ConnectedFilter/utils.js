ae8c6eea343e0214074dcaab2180c4b68d22ddc7
Michael Ingram <mpingram@uchicago.edu>
2020-10-14 15:40:50 -0500

    (PXP-6359) Add Search Filter (#99)
    * Modify guppy to work with searchField
    
    * Update comments
    
    * Clean up code
    
    * Pin g3uicomponent to branch
    
    * Update package-lock.json
    
    * Random change to retrigger jenkins integration tests
    
    * Revert "Random change to retrigger jenkins integration tests"
    
    This reverts commit 393ad4839b606e813ff1a7f308a04e1c5a60ab74.
    
    * Remove unused guppySingleFieldSearch function
    
    * Pin gen3uicomponent to 0.6.0
    
    * Bump package version to 0.9.0
    
    * Fix package version

--- src/components/ConnectedFilter/utils.js
+++ src/components/ConnectedFilter/utils.js
@@ -1,4 +1,5 @@
 import flat from 'flat';
+import { queryGuppyForRawDataAndTotalCounts } from '../Utils/queries';
 
 export const getFilterGroupConfig = (filterConfig) => ({
   tabs: filterConfig.tabs.map((t) => ({
@@ -42,9 +43,92 @@ const capitalizeFirstLetter = (str) => {
   return res.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
 };
 
+// createSearchFilterLoadOptionsFn creates a handler function that loads the search filter's
+// autosuggest options as the user types in the search filter.
+const createSearchFilterLoadOptionsFn = (field, guppyConfig) => (searchString, offset) => {
+  const NUM_SEARCH_OPTIONS = 20;
+  return new Promise((resolve, reject) => {
+    // If searchString is empty return just the first NUM_SEARCH_OPTIONS options.
+    // This allows the client to show default options in the search filter before
+    // the user has started searching.
+    let filter = {};
+    if (searchString) {
+      filter = {
+        search: {
+          keyword: searchString,
+          fields: [field],
+        },
+      };
+    }
+    queryGuppyForRawDataAndTotalCounts(
+      guppyConfig.path,
+      guppyConfig.type,
+      [field],
+      filter,
+      undefined,
+      offset,
+      NUM_SEARCH_OPTIONS,
+      'accessible',
+    )
+      .then((res) => {
+        if (!res.data || !res.data[guppyConfig.type]) {
+          resolve({
+            options: [],
+            hasMore: false,
+          });
+        } else {
+          const results = res.data[guppyConfig.type];
+          const totalCount = res.data._aggregation[guppyConfig.type]._totalCount;
+          resolve({
+            options: results.map((result) => ({ value: result[field], label: result[field] })),
+            hasMore: totalCount > offset + results.length,
+          });
+        }
+      }).catch((err) => {
+        reject(err);
+      });
+  });
+};
+
 export const getFilterSections = (
-  fields, fieldMapping, tabsOptions, initialTabsOptions, adminAppliedPreFilters,
+  fields, searchFields, fieldMapping, tabsOptions,
+  initialTabsOptions, adminAppliedPreFilters, guppyConfig,
 ) => {
+  let searchFieldSections = [];
+  if (searchFields) {
+    // Process searchFields first -- searchFields are special filters that allow the user
+    // to search over all options, instead of displaying all options in a list. This allows
+    // guppy/portal to support filters that have too many options to be displayed in a list.
+    searchFieldSections = searchFields.map((field) => {
+      const overrideName = fieldMapping.find((entry) => (entry.field === field));
+      const label = overrideName ? overrideName.name : capitalizeFirstLetter(field);
+
+      const tabsOptionsFiltered = { ...tabsOptions[field] };
+      if (Object.keys(adminAppliedPreFilters).includes(field)) {
+        tabsOptionsFiltered.histogram = tabsOptionsFiltered.histogram.filter(
+          (x) => adminAppliedPreFilters[field].selectedValues.includes(x.key),
+        );
+      }
+
+      // For searchFields, don't pass all options to the component, only the selected ones.
+      // This allows selected options to appear below the search box once they are selected.
+      let selectedOptions = [];
+      if (tabsOptionsFiltered && tabsOptionsFiltered.histogram) {
+        selectedOptions = getSingleFilterOption(
+          tabsOptionsFiltered,
+          initialTabsOptions ? initialTabsOptions[field] : undefined,
+        );
+      }
+
+      return {
+        title: label,
+        options: selectedOptions,
+        isSearchFilter: true,
+        onSearchFilterLoadOptions: createSearchFilterLoadOptionsFn(field, guppyConfig),
+      };
+    });
+  }
+
   const sections = fields.map((field) => {
     const overrideName = fieldMapping.find((entry) => (entry.field === field));
     const label = overrideName ? overrideName.name : capitalizeFirstLetter(field);
@@ -66,7 +150,7 @@ export const getFilterSections = (
       options: defaultOptions,
     };
   });
-  return sections;
+  return searchFieldSections.concat(sections);
 };
 
 export const excludeSelfFilterFromAggsData = (receivedAggsData, filterResults) => {
