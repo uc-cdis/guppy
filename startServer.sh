a588b839593d0e96b547674e008808abb32c0279
Michael Lukowski <lukowski9@gmail.com>
2023-08-17 14:26:59 -0500

    add mem constraint for node

--- startServer.sh
+++ startServer.sh
@@ -4,4 +4,4 @@ if [[ -z "$DD_TRACE_ENABLED" ]]; then
     export DD_TRACE_ENABLED=false
 fi
 
-node --max-http-header-size 16000 --require dd-trace/init dist/server/server.js
+node --max-old-space-size 3584 --max-http-header-size 16000 --require dd-trace/init dist/server/server.js
