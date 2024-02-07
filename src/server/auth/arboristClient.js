ecfd5f234e1ae2c100fde62d3ea3356fcf7d0ea9
Luca Graglia <graglia01@gmail.com>
2024-02-07 11:07:56 -0600

    using POST auth/mapping instead of GET

--- src/server/auth/arboristClient.js
+++ src/server/auth/arboristClient.js
@@ -12,39 +12,46 @@ class ArboristClient {
     // Make request to arborist for list of resources with access
     const resourcesEndpoint = `${this.baseEndpoint}/auth/mapping`;
     log.debug('[ArboristClient] listAuthorizedResources jwt: ', jwt);
-    const headers = (jwt) ? { Authorization: `bearer ${jwt}` } : {};
-    return fetch(
-      resourcesEndpoint,
-      {
-        method: 'GET',
-        headers,
-      },
-    ).then(
-      (response) => response.json(),
-    ).then(
-      (result) => {
-        const data = {
-          resources: [],
-        };
-        Object.keys(result).forEach((key) => {
-        // logic: you have access to a project if you have the following access:
-        // method 'read' (or '*' - all methods) to service 'guppy' (or '*' - all services)
-        // on the project resource.
-          if (result[key] && result[key].some((x) => (
-            (x.method === 'read' || x.method === '*')
-          && (x.service === 'guppy' || x.service === '*')
-          ))) {
-            data.resources.push(key);
-          }
-        });
-        log.debug('[ArboristClient] data: ', data);
-        return data;
-      },
-      (err) => {
-        log.error(err);
-        throw new CodedError(500, err);
-      },
-    );
+    if (jwt) {
+      // Enforcing the use of the JWT since we are using the arborist POST endpoint. The POST endpoint check for the presence of a token and validates it if present. 
+      // For retrocompatibility if the token is not present will use the clientID or username passed in the body. We want to avoid using the body values since that could expose 
+      // other clients or usernames that are not the logged in user ones.
+      const headers = (jwt) ? { Authorization: `bearer ${jwt}` } : {};
+      return fetch(
+        resourcesEndpoint,
+        {
+          method: 'POST',
+          headers,
+        },
+      ).then(
+        (response) => response.json(),
+      ).then(
+        (result) => {
+          const data = {
+            resources: [],
+          };
+          Object.keys(result).forEach((key) => {
+          // logic: you have access to a project if you have the following access:
+          // method 'read' (or '*' - all methods) to service 'guppy' (or '*' - all services)
+          // on the project resource.
+            if (result[key] && result[key].some((x) => (
+              (x.method === 'read' || x.method === '*')
+            && (x.service === 'guppy' || x.service === '*')
+            ))) {
+              data.resources.push(key);
+            }
+          });
+          log.debug('[ArboristClient] data: ', data);
+          return data;
+        },
+        (err) => {
+          log.error(err);
+          throw new CodedError(500, err);
+        },
+      );
+    } else {
+      return {"resources": []}
+    }
   }
 }
 
