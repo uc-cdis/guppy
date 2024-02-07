0d7661ebb02a0e937f2415b0dffc73616d369a11
grugna <graglia01@gmail.com>
2024-02-07 10:47:42 -0600

    remove comments

--- src/server/auth/arboristClient.js
+++ src/server/auth/arboristClient.js
@@ -15,60 +15,6 @@ class ArboristClient {
     this.baseEndpoint = arboristEndpoint;
   }
 
-  // listAuthorizedResources(jwt) {
-  //   // Make request to arborist for list of resources with access
-  //   const resourcesEndpoint = `${this.baseEndpoint}/auth/mapping`;
-  //   log.debug('[ArboristClient] listAuthorizedResources jwt: ', jwt);
-  //   const headers = (jwt) ? { Authorization: `bearer ${jwt}` } : {};
-  //   return fetch(
-  //     resourcesEndpoint,
-  //     {
-  //       method: 'GET',
-  //       headers: headers,
-  //       timeout: 0,
-  //       agent: new http.Agent({
-  //         keepAlive: true,
-  //         keepAliveMsecs: 10000,
-  //       }),
-  //       // agent: function (_parsedURL) {
-  //       //     if (_parsedURL.protocol == 'http:') {
-  //       //         return httpAgent;
-  //       //     } else {
-  //       //         return httpsAgent;
-  //       //     }
-  //       // }
-  //     },
-  //   ).then(
-  //     (response) => response.json(),
-  //   ).then(
-  //     (result) => {
-  //       const data = {
-  //         resources: [],
-  //       };
-  //       Object.keys(result).forEach((key) => {
-  //       // logic: you have access to a project if you have the following access:
-  //       // method 'read' (or '*' - all methods) to service 'guppy' (or '*' - all services)
-  //       // on the project resource.
-  //         if (result[key] && result[key].some((x) => (
-  //           (x.method === 'read' || x.method === '*')
-  //         && (x.service === 'guppy' || x.service === '*')
-  //         ))) {
-  //           data.resources.push(key);
-  //         }
-  //       });
-  //       log.debug('[ArboristClient] data: ', data);
-
-  //       // TODO remove this line
-  //       log.error('[ArboristClient] data: ', data);
-  //       return data;
-  //     },
-  //     (err) => {
-  //       log.error(err);
-  //       throw new CodedError(500, err);
-  //     },
-  //   );
-  // }
-
   listAuthorizedResources(jwt) {
     // Make request to arborist for list of resources with access
     const resourcesEndpoint = `${this.baseEndpoint}/auth/mapping`;
@@ -111,9 +57,6 @@ class ArboristClient {
           }
         });
         log.debug('[ArboristClient] data: ', data);
-
-        // TODO remove this line
-        log.error('[ArboristClient] data: ', data);
         return data;
       },
       (err) => {
