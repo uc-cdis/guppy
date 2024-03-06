3af1972ffa5cc952399b7a38529b59a67679bdc9
Luca Graglia <graglia01@gmail.com>
2024-03-06 09:05:48 -0600

    remove comment

--- src/server/auth/arboristClient.js
+++ src/server/auth/arboristClient.js
@@ -13,10 +13,6 @@ class ArboristClient {
     const resourcesEndpoint = `${this.baseEndpoint}/auth/mapping`;
     log.debug('[ArboristClient] listAuthorizedResources jwt: ', jwt);
     
-    // The POST endpoint checks for the presence of a token and validates it if present. 
-    // For retrocompatibility if the token is not present will use the clientID or username passed in the body. 
-    // We want to avoid using the body values since that could expose 
-    // other clients or usernames that are not the logged in user ones.
     const headers = (jwt) ? { Authorization: `bearer ${jwt}` } : {};
     return fetch(
       resourcesEndpoint,
