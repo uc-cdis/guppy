a252b786236f31b39ebbc64fb293a30ea37a71fc
Alex VanTol <avantol@uchicago.edu>
2024-03-22 09:27:40 -0500

    chore(logging): add time to graphql log

--- src/server/es/index.js
+++ src/server/es/index.js
@@ -53,14 +53,20 @@ class ES {
       },
     };
     validatedQueryBody.track_total_hits = true;
-    log.info('[ES.query] index, type, query body: ', esIndex, esType, JSON.stringify(validatedQueryBody));
-    return this.client.search({
+
+    var start = new Date().getTime();
+    var searchResult = this.client.search({
       index: esIndex,
       body: validatedQueryBody,
     }).then((resp) => resp.body, (err) => {
       log.error(`[ES.query] error during querying: ${err.message}`);
       throw new Error(err.message);
     });
+    var end = new Date().getTime();
+    var durationInMS = end - start;
+
+    log.info('[ES.query] DurationInMS:' + durationInMS + '. index, type, query body: ', esIndex, esType, JSON.stringify(validatedQueryBody));
+    return searchResult;
   }
 
   /**
