71adf662e3388fc4940fdb034f8bc4769c81424d
grugna <graglia01@gmail.com>
2024-07-05 23:19:13 +0200

    Update utils.js

--- src/server/utils/utils.js
+++ src/server/utils/utils.js
@@ -1,3 +1,4 @@
+import { Buffer } from "buffer";
 import rs from 'jsrsasign';
 import config from '../config';
 import log from '../logger';
@@ -63,10 +64,15 @@ export const validSignature = (req) => {
   try {
     const signature = headerParser.parseSignature(req);
     let data = req.body;
-    data = JSON.stringify(data);
+    let data_encoded = Buffer.from(data, 'utf-8');
 
     const { publicKey } = req.app.locals;
-    isValid = publicKey.verify(data, signature);
+
+    const signature_new = new rs.KJUR.crypto.Signature({ alg: "SHA256withRSA" });
+    signature_new.init(publicKey);
+    signature_new.updateString(data);
+
+    isValid = signature_new.verify(signature);
   } catch (err) {
     log.error('[SIGNATURE CHECK] error when checking the signature of the payload', err);
     return false;
