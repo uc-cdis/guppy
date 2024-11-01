078e99b6878f77db7391b142c778cc041cd3f1bd
Mingfei Shao <mshao1@uchicago.edu>
2024-11-01 17:13:45 -0500

    remove d-trace

--- startServer.sh
+++ startServer.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-if [[ -z "$DD_TRACE_ENABLED" ]]; then
-    export DD_TRACE_ENABLED=false
-fi
-
-node --max-http-header-size 16000 --require dd-trace/init dist/server/server.js
+node --max-http-header-size 16000 --require dist/server/server.js
